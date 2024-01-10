805ac37e8e838e6b5713920f6e4da26dbcbcf2bc
jilcys <cyarkinsari@gmail.com>
2024-01-10 10:09:05 +0300

    Add project files.

---
+++ ibantanimahelper/Main.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+namespace ibantanimahelper
+{
+
+    public class findIban
+    {
+        public string IBANBUL(string iban)
+        {
+           
+            string IBAN = iban;            
+            string trimmedIBAN = Regex.Replace(IBAN, @"\s+", "");
+            string Code;
+            if (trimmedIBAN[6].ToString() == "0")
+            {
+                Code = trimmedIBAN[7].ToString() + trimmedIBAN[8].ToString();
+            }
+            else
+            {
+                Code = trimmedIBAN[6].ToString() + trimmedIBAN[7].ToString() + trimmedIBAN[8].ToString();
+            }
+            var Codes = new Repository();
+            var bankname = Codes.getData();
+            for (int i = 0; i < bankname.Count; i++)
+            {
+                if (Code == bankname.ElementAt(i).Key.ToString())
+                {
+                    return bankname.ElementAt(i).Value;
+                }
+            }
+            return null;
+        }
+        public void test()
+        {
+            var test = new Repository();
+            Console.WriteLine(test.getData());
+        }
+       
+
+    }
+    
+}
---
+++ ibantanimahelper/Repository.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ibantanimahelper
+{
+    public class Repository {
+        public Dictionary<int, string> getData()
+        {
+            var BankCodes = new Dictionary<int, string>()
+            {                
+                {
+                    1,
+                    " TC Merkez Bankası"
+                },
+                {
+                    4,
+                    " İller Bankası"
+                },
+                {
+                    10,
+                    " Ziraat Bankası"
+                },
+                {
+                    12,
+                    " Halk bank"
+                },
+                {
+                    14,
+                    " Sınai Kalkınma Bankası"
+                },
+                {
+                    15,
+                    " Vakıflar Bankası"
+                },
+                {
+                    16,
+                    " Eximbank"
+                },
+                {
+                    17,
+                    " Kalkınma Bankası"
+                },
+                {
+                    29,
+                    " Birleşik Fon Bankası"
+                },
+                {
+                    32,
+                    " Türk Ekonomi Bankası (TEB)"
+                },
+                {
+                    46,
+                    " Akbank"
+                },
+                {
+                    59,
+                    " Şekerbank"
+                },
+                {
+                    62,
+                    " Garanti Bankası"
+                },
+                {
+                    64,
+                    " İş Bankası"
+                },
+                {
+                    67,
+                    " Yapı ve Kredi Bankası"
+                },
+                {
+                    91,
+                    " Arap Türk Bankası"
+                },
+                {
+                    92,
+                    " Citibank"
+                },
+                {
+                    94,
+                    " Bank Mellat"
+                },
+                {
+                    96,
+                    " Turkish Bank"
+                },
+                {
+                    98,
+                    " JPMorgan Chase Bank"
+                },
+                {
+                    99,
+                    " ING Bank"
+                },
+                {
+                    100,
+                    " Adabank"
+                },
+                {
+                    103,
+                    " Fibabanka"
+                },
+                {
+                    108,
+                    " Turkland Bank"
+                },
+                {
+                    109,
+                    " ICBC Turkey Bank"
+                },
+                {
+                    111,
+                    " Finansbank"
+                },
+                {
+                    115,
+                    " Deutsche Bank"
+                },
+                {
+                    116,
+                    " Pasha Yatırım Bankası"
+                },
+                {
+                    121,
+                    " Standard Chartered Yatırım Bankası"
+                },
+                {
+                    122,
+                    " Societe Generale"
+                },
+                {
+                    123,
+                    " HSBC"
+                },
+                {
+                    124,
+                    " Alternatif Bank"
+                },
+                {
+                    125,
+                    " Burgan Bank"
+                },
+                {
+                    129,
+                    " Merrill Lynch Yatırım Bank"
+                },
+                {
+                    132,
+                    " Takasbank"
+                },
+                {
+                    134,
+                    " Denizbank"
+                },
+                {
+                    135,
+                    " Anadolu Bank"
+                },
+                {
+                    137,
+                    " Rabobank"
+                },
+                {
+                    138,
+                    " Dilerbank"
+                },
+                {
+                    139,
+                    " GSD Bank"
+                },
+                {
+                    141,
+                    " Nurol Yatırım Bankası"
+                },
+                {
+                    142,
+                    " Bankpozitif Kredi ve Kalkınma Bankası"
+                },
+                {
+                    143,
+                    " Aktif Yatırım Bankası"
+                },
+                {
+                    146,
+                    " Odea Bank"
+                },
+                {
+                    147,
+                    " Bank of Tokyo-Mitsubishi UFJ Turkey"
+                },
+                {
+                    148,
+                    " Intesa Sanpaolo"
+                },
+                {
+                    203,
+                    " Albaraka Türk Katılım Bankası"
+                },
+                {
+                    205,
+                    " Kuveyt Türk Katılım Bankası"
+                },
+                {
+                    206,
+                    " Türkiye Finans Katılım Bankası"
+                },
+                {
+                    209,
+                    " Ziraat Katılım Bankası"
+                },
+                {
+                    210,
+                    " Vakıf Katılım Bankası"
+                },
+                {
+                    806,
+                    " Merkezi Kayıt Kuruluşu"
+                }
+            };
+            return BankCodes;
+
+
+        }
+
+    }
+    
+}
