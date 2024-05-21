7ced262a553c7c0e04043b00adda48f5fa21892a
cagin.sari <cagin.sari@bhmotelcilik.com>
2024-05-21 10:33:09 +0300

    swift edit

--- ibantanimahelper/Repository.cs
+++ ibantanimahelper/Repository.cs
@@ -243,7 +243,7 @@ namespace ibantanimahelper
                 },
                 {
                     12,
-                    "TRHBTV2A"
+                    "TRHBTR2A"
                 },
                 {
                     14,
